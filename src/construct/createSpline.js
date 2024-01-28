a3c37c5c71d2a03391f734ca36e3ac3a70c5c5fd
Dirk Plate <email@dplate.de>
2024-01-28 18:27:00 +0100

    Better spline algorithm

--- src/construct/createSpline.js
+++ src/construct/createSpline.js
@@ -1,31 +1,95 @@
-const catmullRomSpline = (points, t) => {
-  const i = Math.floor(t);
-  if (i + 3 > points.length - 1) {
-    return points[points.length - 2];
+const makeContinuousTangent = (previous, current, next) => {
+  const distanceToPrevious = {
+    x: previous.x - current.x,
+    y: previous.y - current.y
+  };
+  const distanceToNext = {
+    x: next.x - current.x,
+    y: next.y - current.y
+  };
+  const factorToPrevious = Math.pow(
+    distanceToPrevious.x * distanceToPrevious.x + distanceToPrevious.y * distanceToPrevious.y, 
+    0.5
+  );
+  const factorToNext = Math.pow(
+    distanceToNext.x * distanceToNext.x + distanceToNext.y * distanceToNext.y, 
+    0.5
+  );
+  const factor = {
+    x: (factorToNext * factorToNext * distanceToPrevious.x - factorToPrevious * factorToPrevious * distanceToNext.x),
+    y: (factorToNext * factorToNext * distanceToPrevious.y - factorToPrevious * factorToPrevious * distanceToNext.y)
+  }
+  const previousNormalizer = 3 * factorToNext * (factorToPrevious + factorToNext);
+  const nextNormalizer = 3 * factorToPrevious * (factorToPrevious + factorToNext);
+  return {
+    previous: {
+      x: current.x + (previousNormalizer && factor.x / previousNormalizer),
+      y: current.y + (previousNormalizer && factor.y / previousNormalizer)
+    },
+    next: {
+      x: current.x - (nextNormalizer && factor.x / nextNormalizer),
+      y: current.y - (nextNormalizer && factor.y / nextNormalizer)
+    }
+  };
+};
+
+const createSplineEvaluator = (points) => {
+  if (points.length < 3) {
+    return (t) => ({
+      x: (1 - t) * points[0].x + t * points[1].x,
+      y: (1 - t) * points[0].y + t * points[1].y
+    });
+  }
+  const tangents = [];
+  for (let i = 1; i < points.length - 1; i++) {
+      tangents.push(makeContinuousTangent(points[i - 1], points[i], points[i + 1]));
   }
 
-  const p0 = points[i];
-  const p1 = points[i + 1];
-  const p2 = points[i + 2];
-  const p3 = points[i + 3];
+  return (t) => {
+    const correctedT = Math.max(0, Math.min(points.length - 1, t));
+    const previousIndex = Math.min(Math.floor(correctedT), points.length - 2);
+    const remainder = correctedT - previousIndex;
+    const inverseRemainder = 1 - remainder;
 
-  const remainder = t - i;
+    const previousPoint = points[previousIndex];
+    const currentPoint = points[previousIndex + 1];
 
-  const q0 = (-1 * remainder ** 3) + (2 * remainder ** 2) + (-1 * remainder);
-  const q1 = (3 * remainder ** 3) + (-5 * remainder ** 2) + 2;
-  const q2 = (-3 * remainder ** 3) + (4 * remainder ** 2) + remainder;
-  const q3 = remainder ** 3 - remainder ** 2;
+    if (previousIndex === 0 || previousIndex === points.length - 2) {
+      const tangentPoint = previousIndex === 0 ? tangents[previousIndex].previous : tangents[previousIndex - 1].next;
+      const previousPointWeight = inverseRemainder * inverseRemainder;
+      const tangentWeight = 2 * inverseRemainder * remainder;
+      const currentPointWeight = remainder * remainder;
+      return {
+        x: previousPointWeight * previousPoint.x + tangentWeight * tangentPoint.x + currentPointWeight * currentPoint.x,
+        y: previousPointWeight * previousPoint.y + tangentWeight * tangentPoint.y + currentPointWeight * currentPoint.y
+      };
+    } 
 
-  return {
-    x: 0.5 * ((p0.x * q0) + (p1.x * q1) + (p2.x * q2) + (p3.x * q3)),
-    y: 0.5 * ((p0.y * q0) + (p1.y * q1) + (p2.y * q2) + (p3.y * q3))
-  };
-}
+    const previousTangentPoint = tangents[previousIndex - 1].next;
+    const nextTangentPoint = tangents[previousIndex].previous;
+    const nextPoint = points[previousIndex + 1];
 
-const measureSegment = (points, min, max) => {
+    const previousPointWeight = inverseRemainder * inverseRemainder * inverseRemainder;
+    const previousTangentWeight = 3 * inverseRemainder * inverseRemainder * remainder;
+    const nextTangentWeight = 3 * inverseRemainder * remainder * remainder;
+    const nextPointWeight = remainder * remainder * remainder;
+
+    return {
+      x: previousPointWeight * previousPoint.x + 
+        previousTangentWeight * previousTangentPoint.x + 
+        nextTangentWeight * nextTangentPoint.x + nextPointWeight * nextPoint.x,
+      y: previousPointWeight * previousPoint.y + 
+        previousTangentWeight * previousTangentPoint.y + 
+        nextTangentWeight * nextTangentPoint.y + 
+        nextPointWeight * nextPoint.y
+    };
+  }
+};
+
+const measureSegment = (evaluateSpline, min, max) => {
   const detailPoints = [];
-  for (let t = min; t <= max; t += 0.001) {
-    detailPoints.push(catmullRomSpline(points, t));
+  for (let t = min; t < max; t += 0.001) {
+    detailPoints.push(evaluateSpline(t));
   }
 
   let length = 0;
@@ -38,16 +102,12 @@ const measureSegment = (points, min, max) => {
   return length;
 };
 
-export default (controlPoints) => {
-  const points = [
-    controlPoints[0],
-    ...controlPoints,
-    controlPoints[controlPoints.length - 1]
-  ];
+export default (points) => {
+  const evaluateSpline = createSplineEvaluator(points);
 
   const segmentLengths = [0];
-  for (let i = 0; i < points.length - 2; i++){
-    segmentLengths.push(measureSegment(points, i, i + 1));
+  for (let i = 0; i < points.length - 1; i++) {
+    segmentLengths.push(measureSegment(evaluateSpline, i, i + 1));
   }
   const length = segmentLengths.reduce((sum, length) => sum + length, 0);
 
@@ -64,7 +124,7 @@ export default (controlPoints) => {
       const segmentLength = segmentLengths[segmentIndex + 1];
       const remainingLength = meter - totalSegmentLength;
       const fraction = remainingLength / segmentLength;
-      return catmullRomSpline(points, segmentIndex + fraction);
+      return evaluateSpline(segmentIndex + fraction);
     }
   };
 };

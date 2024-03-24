5931c21330ac8bd307953dda0d6b1225f47a6c40
Dirk Plate <email@dplate.de>
2024-03-24 13:34:09 +0100

    Fix gradient calculation

--- src/construct/route/drawSection.js
+++ src/construct/route/drawSection.js
@@ -19,26 +19,23 @@ const splitSection = (section) => {
   
   const start = section.corners[0].pixels;
   const end = section.corners[section.corners.length - 1].pixels;
-  const startDistance = section.startDistance || 0;
+  section.corners[0].startDistance = section.corners[0].startDistance || 0;
   const splitCorner = {
     index: 0,
     lineDistance: 0,
-    startDistance
   };
-  let currentStartDistance = startDistance;
   section.corners.forEach((corner, index) => {
     if (index > 0) {
-      const previousPixels = section.corners[index - 1].pixels;
-      currentStartDistance += Math.sqrt(
-        (corner.pixels.x - previousPixels.x) ** 2 + 
-        (corner.pixels.y - previousPixels.y) ** 2
+      const previousCorner = section.corners[index - 1];
+      corner.startDistance = previousCorner.startDistance + Math.sqrt(
+        (corner.pixels.x - previousCorner.pixels.x) ** 2 + 
+        (corner.pixels.y - previousCorner.pixels.y) ** 2
       );
     }
     const distance = calculateDistanceFromLine(start, end, corner.pixels);
     if (distance > splitCorner.lineDistance) {
       splitCorner.index = index;
       splitCorner.lineDistance = distance;
-      splitCorner.startDistance = currentStartDistance;
     }
   });
   if (splitCorner.lineDistance < maxJitterPixelDistance) {
@@ -48,33 +45,31 @@ const splitSection = (section) => {
   return [
     {
       type: section.type,
-      corners: section.corners.slice(0, splitCorner.index + 1),
-      startDistance
+      corners: section.corners.slice(0, splitCorner.index + 1)
     },
     {
       type: section.type,
-      corners: section.corners.slice(splitCorner.index),
-      startDistance: splitCorner.startDistance
+      corners: section.corners.slice(splitCorner.index)
     }
-  ].reduce((splitSections, section) => {
-    section.visible = section.corners.some(corner => corner.visible);
+  ].reduce((splitSections, subSection) => {
+    subSection.visible = subSection.corners.some(corner => corner.visible);
     return [
       ...splitSections,
-      ...splitSection(section)
+      ...splitSection(subSection)
     ];
   }, [])
 };
 
 const drawSubSection = (context, section) => {
-  const firstPixels = section.corners[0].pixels;
-  const lastPixels = section.corners[section.corners.length - 1].pixels;
+  const firstCorner = section.corners[0];
+  const lastCorner = section.corners[section.corners.length - 1];
   
   context.setLineDash([]);
   context.beginPath();
-  context.moveTo(firstPixels.x, firstPixels.y);
-  let previousUsedPixels = firstPixels;
+  context.moveTo(firstCorner.pixels.x, firstCorner.pixels.y);
+  let previousUsedPixels = firstCorner.pixels;
   
-  const gradient = context.createLinearGradient(firstPixels.x, firstPixels.y, lastPixels.x, lastPixels.y); 
+  const gradient = context.createLinearGradient(firstCorner.pixels.x, firstCorner.pixels.y, lastCorner.pixels.x, lastCorner.pixels.y); 
   gradient.addColorStop(0, section.corners[0].color);
   section.corners.forEach((corner, index) => {
     if (index > 0 && (
@@ -82,7 +77,11 @@ const drawSubSection = (context, section) => {
       Math.abs(previousUsedPixels.x - corner.pixels.x) > detailPixelDistance || 
       Math.abs(previousUsedPixels.y - corner.pixels.y) > detailPixelDistance)) {
       context.lineTo(corner.pixels.x, corner.pixels.y);
-      gradient.addColorStop(index / (section.corners.length - 1), corner.color);
+      gradient.addColorStop(
+        (corner.startDistance - firstCorner.startDistance) / 
+        (lastCorner.startDistance - firstCorner.startDistance), 
+        corner.color
+      );
       previousUsedPixels = corner.pixels;
     }
   });
@@ -96,7 +95,7 @@ const drawSubSection = (context, section) => {
 
   if (section.type === ROUTE_TYPE_TUNNEL) {
     const lineDash = [5, 15];
-    context.lineDashOffset = section.startDistance % (lineDash[0] + lineDash[1]);
+    context.lineDashOffset = section.corners[0].startDistance % (lineDash[0] + lineDash[1]);
     context.setLineDash(lineDash);
   } else {
     context.setLineDash([]);
@@ -111,5 +110,7 @@ const drawSubSection = (context, section) => {
 
 export default (context, section) => {
   const subSections = splitSection(section);
-   subSections.forEach((subSection) => drawSubSection(context, subSection));
+  subSections.forEach((subSection) => {
+    drawSubSection(context, subSection)
+  });
 };

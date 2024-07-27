48431fea624367fdab1c6b5b0a2f048cae18d079
Dirk Plate <email@dplate.de>
2024-07-27 20:02:09 +0200

    More decoration for desk

--- src/menu/createMenuLayout.js
+++ src/menu/createMenuLayout.js
@@ -14,6 +14,30 @@ export default (system) => {
   account.id = 'account';
   desk.appendChild(account);
 
+  for (let i = 0; i <= 2; i++) {
+    const stain = document.createElement('div');
+    stain.className = 'stain';
+    stain.style.right = `${30 + Math.random() * 15}px`;
+    stain.style.top = `${30 + Math.random() * 15}px`;
+    stain.style.opacity = 1 - Math.random() / 5 * (2 - i);
+    stain.style.transform = `rotate(${Math.random()}turn) scale(${-1 + i%2 * 2})`;
+    desk.appendChild(stain);
+  }
+
+  const bridge = document.createElement('div');
+  bridge.id = 'bridge';
+  bridge.style.left = `calc(600px + ${Math.random()} * (100vw - 700px))`;
+  bridge.style.bottom = `${-10 + Math.random() * 50}vh`;
+  bridge.style.transform = `rotate(${-0.05 + Math.random() * 0.1}turn)`;
+  desk.appendChild(bridge);
+
+  const ruler = document.createElement('div');
+  ruler.id = 'ruler';
+  ruler.style.left = `calc(600px + ${Math.random()} * (100vw - 800px))`;
+  ruler.style.bottom = `${-10 + Math.random() * 80}vh`;
+  ruler.style.transform = `rotate(${Math.random()}turn)`;
+  desk.appendChild(ruler);
+
   document.body.appendChild(desk);
 
   const shadow = document.createElement('div');
